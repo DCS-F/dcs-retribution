617c6576d96e00cad9f1bf738395fffcf09fabb8
MetalStormGhost <ghosti@smytky.org>
2023-10-22 00:16:50 +0300

    Replaced some cruise missiles with bombs on the H-6J loadouts for early cold war scenarios.

--- resources/customized_payloads/H-6J.lua
+++ resources/customized_payloads/H-6J.lua
@@ -6,31 +6,35 @@ local unitPayloads = {
 			["name"] = "Retribution DEAD",
 			["pylons"] = {
 				[1] = {
+					["CLSID"] = "DIS_H6_250_2_N24",
+					["num"] = 8,
+				},
+				[2] = {
 					["CLSID"] = "DIS_AKG_DLPOD",
 					["num"] = 7,
 				},
-				[2] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[3] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 6,
 				},
-				[3] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[4] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 5,
 				},
-				[4] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[5] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 4,
 				},
-				[5] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[6] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 3,
 				},
-				[6] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[7] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 2,
 				},
-				[7] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[8] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 1,
 				},
 			},
@@ -83,31 +87,35 @@ local unitPayloads = {
 			["name"] = "Retribution Strike",
 			["pylons"] = {
 				[1] = {
+					["CLSID"] = "DIS_H6_250_2_N24",
+					["num"] = 8,
+				},
+				[2] = {
 					["CLSID"] = "DIS_AKG_DLPOD",
 					["num"] = 7,
 				},
-				[2] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[3] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 6,
 				},
-				[3] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[4] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 5,
 				},
-				[4] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[5] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 4,
 				},
-				[5] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[6] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 3,
 				},
-				[6] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[7] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 2,
 				},
-				[7] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[8] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 1,
 				},
 			},
@@ -119,31 +127,35 @@ local unitPayloads = {
 			["name"] = "Retribution BAI",
 			["pylons"] = {
 				[1] = {
+					["CLSID"] = "DIS_H6_250_2_N24",
+					["num"] = 8,
+				},
+				[2] = {
 					["CLSID"] = "DIS_AKG_DLPOD",
 					["num"] = 7,
 				},
-				[2] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[3] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 6,
 				},
-				[3] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[4] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 5,
 				},
-				[4] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[5] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 4,
 				},
-				[5] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[6] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 3,
 				},
-				[6] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[7] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 2,
 				},
-				[7] = {
-					["CLSID"] = "DIS_DF4A_KD20",
+				[8] = {
+					["CLSID"] = "DIS_MER6_250_3_N6",
 					["num"] = 1,
 				},
 			},
