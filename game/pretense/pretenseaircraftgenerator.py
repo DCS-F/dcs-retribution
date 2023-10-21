19e9bf95ca707ae8ff4e81f4cf391fee7a7a0d31
MetalStormGhost <ghosti@smytky.org>
2023-10-21 15:23:56 +0300

    Removed unnecessary code from pretenseaircraftgenerator.py Reduced the number of cargo planes to 2 per side.

--- game/pretense/pretenseaircraftgenerator.py
+++ game/pretense/pretenseaircraftgenerator.py
@@ -57,7 +57,7 @@ PRETENSE_BARCAP_FLIGHTS_PER_CP = 2
 PRETENSE_AI_AIRCRAFT_PER_FLIGHT = 2
 PRETENSE_AI_AWACS_PER_FLIGHT = 1
 PRETENSE_AI_TANKERS_PER_FLIGHT = 1
-PRETENSE_AI_CARGO_PLANES_PER_SIDE = 8
+PRETENSE_AI_CARGO_PLANES_PER_SIDE = 2
 PRETENSE_PLAYER_AIRCRAFT_PER_FLIGHT = 1
 PRETENSE_PLAYER_FLIGHTS_PER_TYPE = 2
 
@@ -754,47 +754,15 @@ class PretenseAircraftGenerator:
         """Creates and configures the flight group in the mission."""
         if not country.unused_onboard_numbers:
             country.reset_onboard_numbers()
-            group = PretenseFlightGroupSpawner(
-                flight,
-                country,
-                self.mission,
-                self.helipads,
-                self.ground_spawns_roadbase,
-                self.ground_spawns,
-                self.mission_data,
-            ).create_flight_group()
-        else:
-            if flight.client_count > 0:
-                if flight.flight_type == FlightType.CAS:
-                    for conflict in self.game.theater.conflicts():
-                        flight.package.target = conflict
-                        break
-                elif (
-                    flight.flight_type == FlightType.STRIKE
-                    or flight.flight_type == FlightType.BAI
-                ):
-                    for cp in self.game.theater.closest_opposing_control_points():
-                        if cp.coalition == flight.coalition:
-                            continue
-                        for mission_target in cp.ground_objects:
-                            flight.package.target = mission_target
-                elif (
-                    flight.flight_type == FlightType.OCA_RUNWAY
-                    or flight.flight_type == FlightType.OCA_AIRCRAFT
-                ):
-                    for cp in self.game.theater.controlpoints:
-                        if cp.coalition == flight.coalition or not isinstance(
-                            cp, Airfield
-                        ):
-                            continue
-                        flight.package.target = cp
-                elif flight.flight_type == FlightType.DEAD:
-                    for cp in self.game.theater.controlpoints:
-                        if cp.coalition == flight.coalition:
-                            continue
-                        for ground_object in cp.ground_objects:
-                            is_ewr = isinstance(ground_object, EwrGroundObject)
-                            is_sam = isinstance(ground_object, SamGroundObject)
+        group = PretenseFlightGroupSpawner(
+            flight,
+            country,
+            self.mission,
+            self.helipads,
+            self.ground_spawns_roadbase,
+            self.ground_spawns,
+            self.mission_data,
+        ).create_flight_group()
 
         control_points_to_scan = (
             list(self.game.theater.closest_opposing_control_points())
