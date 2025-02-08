7a3552ab1e459aaf1622bd11bfb135e0c324ff07
MetalStormGhost <ghosti@smytky.org>
2025-02-08 11:56:39 +0200

    Added banners, icons, loadouts and unit yamls by Biggus for new Cold War Assets added in mod version 1.2.

--- game/factions/faction.py
+++ game/factions/faction.py
@@ -570,7 +570,21 @@ class Faction:
             self.remove_preset("LvS-103 Rb103B")
             self.remove_preset("LvS-103 Rb103B Mobile")
         if not mod_settings.coldwarassets:
-            self.remove_aircraft("EA_6B")
+            self.remove_aircraft("B_47")
+            self.remove_aircraft("Tu-4K")
+            self.remove_aircraft("Tu-16")
+            self.remove_aircraft("tu_22D")
+            self.remove_aircraft("tu_22KD")
+            self.remove_aircraft("B_47")
+            self.remove_aircraft("B_58")
+            self.remove_aircraft("Tu-4K")
+            self.remove_aircraft("Tu-16")
+            self.remove_aircraft("tu_22D")
+            self.remove_aircraft("tu_22KD")
+            self.remove_aircraft("Tu_95K")
+            self.remove_aircraft("Tu_126")
+            self.remove_aircraft("Tu_128M")
+            self.remove_aircraft("Yak_28")
         # SWPack
         if not mod_settings.SWPack:
             self.remove_aircraft("AWINGA")
