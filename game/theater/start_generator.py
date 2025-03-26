744754c4a1995c5664487493ba22a5a59d5e858a
Drexyl <mcourt@konekt.com.au>
2025-03-27 06:40:07 +1100

    Add Cowboy's E-7A Wedgetail AI Mod (#495)

--- game/theater/start_generator.py
+++ game/theater/start_generator.py
@@ -77,6 +77,7 @@ class ModSettings:
     a4_skyhawk: bool = False
     a6a_intruder: bool = False
     a7e_corsair2: bool = False
+    e7a_wedgetail: bool = False
     f4bc_phantom: bool = False
     f15d_baz: bool = False
     f_15_idf: bool = False
--- qt_ui/main.py
+++ qt_ui/main.py
@@ -338,6 +338,7 @@ def create_game(
             a6a_intruder=False,
             a7e_corsair2=False,
             fa_18d=False,
+            e7a_wedgetail=False,
             fa_18efg=False,
             f4bc_phantom=False,
             f22_raptor=False,
