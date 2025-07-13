27adf91ef581465ba820fa71377469d8567b4471
Raffson <Raffson@users.noreply.github.com>
2025-07-13 13:46:57 +0200

    Overrride WFMR-window's reject to properly reset game-state

--- qt_ui/windows/QWaitingForMissionResultWindow.py
+++ qt_ui/windows/QWaitingForMissionResultWindow.py
@@ -234,6 +234,10 @@ class QWaitingForMissionResultWindow(QDialog):
             self.on_debriefing_update(
                 self.sim_controller.debrief_current_state(Path(file[0]), force_end=True)
             )
+            
+    def reject(self):
+        self.reset_game_state()
+        super().reject()
 
     def reset_game_state(self):
         self.sim_controller.set_game(self.game)
