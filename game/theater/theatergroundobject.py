6c9cf394f0a1292dcd7b4ae9a5f81fb8b2baa5c3
Eclipse/Druss99 <dalton.w.russell@gmail.com>
2025-10-13 22:37:50 -0400

    show PRESENT status for neutral TGOs

--- game/theater/theatergroundobject.py
+++ game/theater/theatergroundobject.py
@@ -88,6 +88,8 @@ class TheaterGroundObject(MissionTarget, SidcDescribable, ABC):
 
     @property
     def sidc_status(self) -> Status:
+        if self.control_point.captured.is_neutral:
+            return Status.PRESENT
         if self.is_dead:
             return Status.PRESENT_DESTROYED
         elif self.dead_units:
@@ -565,6 +567,8 @@ class SamGroundObject(IadsGroundObject):
 
     @property
     def sidc_status(self) -> Status:
+        if self.control_point.captured.is_neutral:
+            return Status.PRESENT
         if self.is_dead:
             return Status.PRESENT_DESTROYED
         elif self.dead_units:
