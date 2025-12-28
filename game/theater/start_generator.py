b2b88a4466519b1ed33e599cd22e39d76488acc3
MetalStormGhost <ghosti@smytky.org>
2025-12-28 11:24:52 +0200

    Removed duplicate mirage_3: bool = False

--- game/theater/start_generator.py
+++ game/theater/start_generator.py
@@ -99,7 +99,6 @@ class ModSettings:
     mirage_3: bool = False
     sk_60: bool = False
     mam: bool = False
-    mirage_3: bool = False
     t45_goshawk: bool = False
     hawk_t1a: bool = False
     super_etendard: bool = False
